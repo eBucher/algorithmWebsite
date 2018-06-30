69eff461dcd37f2546b20af9502faf17afec63ea
erich <bucher.erich@gmail.com>
2018-06-30 15:25:08 -0700

    Add AlgorithmSideBar component

--- frontend/src/pages/AlgorithmPage.js
+++ frontend/src/pages/AlgorithmPage.js
@@ -2,25 +2,22 @@ import React, {Component} from 'react';
 import DrawArea from 'components/drawArea/DrawArea.js';
 import CodeBox from 'components/codeBox/CodeBox.js';
 import ExplanationBox from 'components/explanationBox/ExplanationBox.js';
-import StepManager from 'components/stepManager/StepManager.js';
-import BinarySearchInput from 'pages/binarySearch/BinarySearchInput.js';
 import Display from 'components/display/Display.js';
-import AlgorithmInputForm from 'components/algorithmInputForm/AlgorithmInputForm.js';
+import AlgorithmSideBar from 'components/algorithmSideBar/AlgorithmSideBar.js';
 
 class AlgorithmPage extends React.Component {
     render(){
         return(
             <div id="AlgorithmContainer">
 				<div class="algorithmVisualization">
-					<div class="algorithmSidebar">
-						<h1 class="pageTitle lightPrimary">{this.props.algorithmName}</h1>
-                        <div class="inputArea">
-                            <AlgorithmInputForm model={this.props.inputModel} />
-                        </div>
-                        <div class="stepManagerArea">
-						    <StepManager value ={this.props.currentStepNum} numSteps={this.props.numSteps} enabled={this.props.started} parent={this.props.parent}/>
-                        </div>
-                    </div>
+                    <AlgorithmSideBar
+                        algorithmName = {this.props.algorithmName}
+                        inputModel = {this.props.inputModel}
+                        currentStepNum = {this.props.currentStepNum}
+                        numSteps = {this.props.numSteps}
+                        started = {this.props.started}
+                        parent = {this.props.parent}
+                    />
 					<Display>
 						<DrawArea w={this.props.areaWidth} h={this.props.areaHeight} displayedPieces={this.props.piecesToShow}/>
 					</Display>

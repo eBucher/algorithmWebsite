2df9ea96311a65c601ba1e85f4dc0664764fd7f3
erich <bucher.erich@gmail.com>
2018-07-21 16:59:02 -0700

    Add navigation to top of page

--- frontend/src/components/headerBar/HeaderBar.js
+++ frontend/src/components/headerBar/HeaderBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import 'components/headerBar/HeaderBar.css';
+import {Dropdown, Menu} from "semantic-ui-react";
+import {Link} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 /** A header bar for the top of the page */
 class HeaderBar extends React.Component {
@@ -10,7 +13,15 @@ class HeaderBar extends React.Component {
 				</div>
 				<div className="headerBar">
 					<p><font className="orangeText">&#8226;</font> SeeTheSteps.com</p>
-					<p className="rightLinks"> Home  | About | Feedback</p>
+					<Dropdown item text='Other Algorithms'>
+				      <Dropdown.Menu direction="left">
+					  <Dropdown.Header icon='search' content='Searching' />
+				        <Dropdown.Item as={Link} to="/algorithms/binarySearch">Binary Search</Dropdown.Item>
+						<Dropdown.Item as={Link} to="/algorithms/LinearSearch">Linear Search</Dropdown.Item>
+						<Dropdown.Header icon='sort amount up' content='Sorting' />
+						<Dropdown.Item as={Link} to="/algorithms/bubbleSort">Bubble Sort</Dropdown.Item>
+				      </Dropdown.Menu>
+				    </Dropdown>
 				</div>
 				<div className="headerMargin">
 				</div>
--- frontend/src/pages/Test.js
+++ frontend/src/pages/Test.js
@@ -3,13 +3,22 @@ import "./styles.css";
 import BackwardIcon from "assets/BackwardIcon.svg";
 import ForwardIcon from "assets/WarningIcon.svg";
 import CommentIcon from "assets/CommentIcon.svg";
-import {Button} from "semantic-ui-react";
+import {Dropdown, Menu} from "semantic-ui-react";
+import {Link} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 class Test extends React.Component{
 
 	render(){
+
 		return(
-			<Button circular icon="settings"/>
+		    <Dropdown item text='Other Algorithms'>
+		      <Dropdown.Menu direction="left">
+		        <Dropdown.Item as={Link} to="algorithms/binarySearch">Binary Search</Dropdown.Item>
+		        <Dropdown.Item as={Link} to="algorithms/bubbleSort">Bubble Sort</Dropdown.Item>
+				<Dropdown.Item as={Link} to="algorithms/LinearSearch">Linear Search</Dropdown.Item>
+		      </Dropdown.Menu>
+		    </Dropdown>
+
 		)
 	}
 

088e81a208db84fc9a65ffac831df3b045f6d839
erich <bucher.erich@gmail.com>
2018-07-26 12:55:00 -0700

    Split up CodeBox tests

--- frontend/src/components/codeBox/CodeBox.test.js
+++ frontend/src/components/codeBox/CodeBox.test.js
@@ -8,34 +8,52 @@ import configureStore from 'redux-mock-store';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Testing <CodeBox/>', () => {
-    var code = [
+    const code = [
         "zero",
         "    one",
-        "        two"
+        "        two",
+        "      three",
     ];
-    beforeEach(() => {
-
-    });
 
 
     //Expected: each set of four spaces should be replaced with space unicode chars.
-    test('CodeBox: cleanLine()', () => {
-        const wrapper = shallow(<CodeBox linesOfCode={code} highlightedLines={1}/>).instance();
+    test('CodeBox: cleanLine() when there are no spaces', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code}/>).instance();
         expect(wrapper.cleanLine(code[0])).toEqual("zero");
-        expect(wrapper.cleanLine(code[1])).toEqual("\u00A0\u00A0\u00A0\u00A0one");
-        expect(wrapper.cleanLine(code[2])).toEqual("\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0two");
     });
 
+    test('CodeBox: cleanLine() when there are four spaces', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code}/>).instance();
+        expect(wrapper.cleanLine(code[1])).toEqual("\u00A0\u00A0\u00A0\u00A0one");
+    });
 
-    //Expected: each set of four spaces should be replaced with space unicode chars.
-    test('CodeBox: buildLine()', () => {
-        const wrapper = shallow(<CodeBox linesOfCode={code} highlightedLines={1}/>).instance();
-        var output = wrapper.generateLinesOfCode();
-        var expected = [
-            <div style={{"whiteSpace": "pre-line"}}>zero</div>,
-            <div style={{"backgroundColor": "#ffc947", "whiteSpace": "pre-line"}}>    one</div>,
-            <div style={{"whiteSpace": "pre-line"}}>        two</div>
-        ]
-        expect(output).toEqual(expected);
+    test('CodeBox: cleanLine() when there are eight spaces', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code}/>).instance();
+        var expected = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0two";
+        expect(wrapper.cleanLine(code[2])).toEqual(expected);
+    });
+
+    test('CodeBox: cleanLine() when the number of spaces mod 4 is not 0', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code}/>).instance();
+        expect(wrapper.cleanLine(code[3])).toEqual("\u00A0\u00A0\u00A0\u00A0  three");
+    });
+
+    test('CodeBox: buildLine() that is a highlighted line', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code} highlightedLine={1}/>).instance();
+        var output = wrapper.buildLine(1);
+        console.log(output.props.style);
+        expect(output.props.style.backgroundColor).not.toBeUndefined();
+    });
+
+    test('CodeBox: buildLine() that is not a highlighted line', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code} highlightedLine={1}/>).instance();
+        var output = wrapper.buildLine(2);
+        console.log(output.props.style);
+        expect(output.props.style.backgroundColor).toBeUndefined();
+    });
+
+    test('CodeBox: render()', () => {
+        const wrapper = shallow(<CodeBox linesOfCode={code} highlightedLine={1}/>);
+        expect(wrapper.exists()).toEqual(true);
     });
 });

42ed8c2b152e9d93e7a23e596cfd645690fa1d35
erich <bucher.erich@gmail.com>
2018-08-03 13:11:29 -0700

    Add new shape components to insertion sort.

--- frontend/src/pages/insertionSort/InsertionSortDraw.js
+++ frontend/src/pages/insertionSort/InsertionSortDraw.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Square from 'components/drawArea/shapes/Square.js';
-import Pointer from 'components/drawArea/shapes/Pointer.js';
-import BooleanBox from 'components/drawArea/shapes/BooleanBox.js';
-import Coord from 'components/drawArea/math/Coord.js';
-import {CONTENT_SQUARE, SMALL_POINTER, IF_STATEMENT, LOOP_CONTINUATION} from 'components/drawArea/shapes/Presets.js';
+import ArrayVisual from 'components/dataStructures/arrayVisual/ArrayVisual.js';
+import BooleanBox from 'components/shapes/booleanBox/BooleanBox.js';
+import Square, {squareDefaultProps} from 'components/shapes/square/Square.js';
 
 class InsertionSortDraw {
 
@@ -15,44 +13,70 @@ class InsertionSortDraw {
 		this.areaHeight = areaHeight;
 	}
 
-    /** Returns the top left coordinate of where the element at the given
-        index should be drawn. */
-    boxLocation = (currentElementIndex, numElements) => {
-        var drawAreaCenterX = this.areaWidth /2;
-        var squareSize = CONTENT_SQUARE().size;
-
-        var x = (drawAreaCenterX - numElements/2.0*squareSize) +
-            squareSize * currentElementIndex;
-        var y = 100;
-        return new Coord(x, y);
-    }
 
     /** Given the current step object and the index of the element that will
         be drawn, the function returns what color the element's box should
         be. */
     getBoxColor = (currentStepState, currentElementIndex) => {
-        if(currentStepState.i - 1 > currentElementIndex){
+        if(currentStepState.i - 1 > currentElementIndex ||
+			currentStepState.highlightedLines === 11){
             return "green";
         }
         return "white";
     }
 
 
-    /** Adds all of the boxes with the numbers to elementsToDraw. */
-    addBoxes = (elementsToDraw, currentStepState) => {
-        for(var i = 0; i < currentStepState.elements.length; i++){
-            var r = new Square(CONTENT_SQUARE());
-            r.topLeft = this.boxLocation(i, currentStepState.elements.length);
-            r.centerText = currentStepState.elements[i];
-            r.topText = i;
-            r.color = this.getBoxColor(currentStepState, i);
-            elementsToDraw.push(r);
-        }
-    }
+	/** Adds the loop box and key box to elementsToDraw */
+	addBottomBoxes = (elementsToDraw, currentStepState) => {
+		elementsToDraw.push(<BooleanBox
+			topText="Continue loop?"
+			status={currentStepState.loopBox}
+			center={{x: this.areaWidth/2 - squareDefaultProps.size*2, y: 275}}
+		/>);
+		elementsToDraw.push(<Square
+			topText="value of key"
+			centerText={currentStepState.key}
+			center={{x: this.areaWidth/2 + squareDefaultProps.size*2, y: 275}}
+		/>);
+	}
+
+
+	/** @returns an object that can be used as an ArrayVisual pointers prop. Contains
+		info for the i and j pointers. */
+	getPointers = (currentStepState) => {
+		var ptrs = [];
+		if(currentStepState.i !== null && currentStepState.i < currentStepState.elements.length){
+			ptrs.push({index: currentStepState.i, text: "i", position: "TOP"});
+		}
+		if(currentStepState.j !== null && currentStepState.j >= 0){
+			ptrs.push({index: currentStepState.j, text: "j", position: "BOTTOM"});
+		}
+		return ptrs;
+	}
+
+
+	/** Adds an ArrayVisual component to elementsToDraw that will render the
+		array in the algorithm and the position of i. */
+	addArrayTo = (elementsToDraw, currentStepState) => {
+		var arrayModel = [];
+		for(var i = 0; i < currentStepState.elements.length; i++){
+			arrayModel.push({
+				value: currentStepState.elements[i],
+				color: this.getBoxColor(currentStepState, i)
+			});
+		}
+		elementsToDraw.push(<ArrayVisual
+			arrayModel={arrayModel}
+			pointers={this.getPointers(currentStepState)}
+			center={{x: this.areaWidth/2, y: 125}}
+		/>);
+	}
+
 
     visualizeAlgorithm = (currentStepState) => {
         var elements = [];
-        this.addBoxes(elements, currentStepState);
+        this.addArrayTo(elements, currentStepState);
+		this.addBottomBoxes(elements, currentStepState);
         return elements;
     }
 }
